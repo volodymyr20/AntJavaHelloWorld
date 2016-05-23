be302e8cb46c34fe03ed6da19a70d13908d0ba7a
volodymyr20 <volodymyrbekesha@gmail.com>
2016-05-23 21:46:46 +0200

    first commit

---
+++ src/helloWorld/HelloWorld.java
@@ -0,0 +1,13 @@
+package helloWorld;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
